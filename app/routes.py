87cb68ed7592f9174bc36cb30a2f1e219eab1927
BlueberryCode <BlueberryCode@outlook.com>
2021-03-22 19:04:16 -0500

    Works after Section 5.5 (Logging Users In)

--- app/routes.py
+++ app/routes.py
@@ -1,33 +1,51 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
+from flask_login import login_user, current_user
+from app import app, db
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {"username": "Daphne"}
     posts = [
         {
-            "author": {"username": "Anthony"},
-            "body": "The Duke of Hastings is a rake.  He's not suitable for you."
+            'author': {'username': 'Anthony'},
+            'body': 'The Duke of Hastings is a rake.  He is not suitable for you.'
         },
         {
-            "author": {"username": "Daphne"},
-            "body": "I propose an agreement.  You pretend to court me.  That will give you peace from the mamas of the ton.  Having a duke pursue me will allow me to attract more suitors."
+            'author': {'username': 'Daphne'},
+            'body': 'I propose an agreement.  You pretend to court me.  That will give you peace from the mamas of the ton.  Having a duke pursue me will allow me to attract more suitors.'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('We received login request for user {}, remember_me {}'.format(
-            form.username.data, form.remember_me.data
-        ))
+    # If user currently log in, don't let them log in a second time
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
+
+    # Display form for a user who's not yet logged in
+    form = LoginForm()
+
+    # Valid form
+    if form.validate_on_submit():
+
+        user = User.query.filter_by(username=form.username.data).first()
+
+        # Valid username and password
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect(url_for('index'))
+
+        # Invalid username or password
+        else:
+            flash("Invalid username or password")
+            return redirect(url_for('login'))
+
+    # Invalid form
     return render_template('login.html', title='Login', form=form)
 
 
